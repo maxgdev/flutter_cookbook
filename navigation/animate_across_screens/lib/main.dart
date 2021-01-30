45637fd5fe7b6aa222a1dfe866851b13184633e0
maxgdev <max@maxgdev.net>
2021-01-30 21:54:16 +0000

    Changed code to Send data to new (detail) screen. Added 'send_data_to_new_screen.dart'

--- navigation/animate_across_screens/lib/main.dart
+++ navigation/animate_across_screens/lib/main.dart
@@ -1,90 +1,88 @@
-// Return data from a screen
-// https://flutter.dev/docs/cookbook/navigation/returning-data
-// return_data_from_screen.dart
+// Send data to a new screen
+// https://flutter.dev/docs/cookbook/navigation/passing-data
+// send_data_to_new_screen.dart
+//
+// Alternative method is to pass the arguments using RouteSettings
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+// 1. Define a todo class.
+// Define the ToDo object class OUTSIDE of main widget
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
 
 void main() {
   runApp(MaterialApp(
-    title: 'Returning Data',
-    home: HomeScreen(),
+    title: 'Passing Data',
+    home: TodosScreen(
+      // 2. Display a list of todos
+      todos: List.generate(
+        20,
+        (i) => Todo(
+          'Todo $i',
+          'A description of what needs to be done for Todo $i',
+        ),
+      ),
+    ),
   ));
 }
 
-class HomeScreen extends StatelessWidget {
+class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosScreen({Key key, @required this.todos}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Returning Data Demo'),
+        title: Text('Todos'),
       ),
-      body: Center(child: SelectionButton()),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-class SelectionButton extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  // In the constructor, require a Todo.
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: Text('Pick an option, any option!'),
-    );
-  }
-
-  // A method that launches the SelectionScreen and awaits the result from
-  // Navigator.pop.
-  _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => SelectionScreen()),
-    );
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result.
-    Scaffold.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text("$result")));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pick an option'),
+        title: Text(todo.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Yep!" as the result.
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Nope!" as the result.
-                  Navigator.pop(context, 'Nope.');
-                },
-                child: Text('Nope.'),
-              ),
-            )
-          ],
-        ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
       ),
     );
   }
