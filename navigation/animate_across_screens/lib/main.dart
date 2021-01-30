b66014fd30539f762a49692fa46d82af0e12bf77
maxgdev <max@maxgdev.net>
2021-01-30 18:20:14 +0000

    Navigating to details page using gestureDetector

---
+++ navigation/animate_across_screens/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animate a widget across screens',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return DetailScreen();
+          }));
+        },
+        child: Image.network(
+          'https://picsum.photos/250?image=9',
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: detailWidget,
+      ),
+    );
+  }
+}
+
+Widget detailWidget = Column(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    Text('Details page for image selected'),
+    Center(
+      child: Image.network('https://picsum.photos/250?image=9'),
+    ),
+  ],
+);
+
