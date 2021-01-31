6649c152eb82dd46a48bbf591b37869f715137fb
maxgdev <max@maxgdev.net>
2021-01-31 16:53:03 +0000

    Renamed main.dart to 'create_horizontal_list.dart'

---
+++ lists/lib/create_horizontal_list.dart
@@ -0,0 +1,57 @@
+// Create a horizontal list
+// https://flutter.dev/docs/cookbook/lists/horizontal-list
+// create_horizontal_list.dart
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Horizontal List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView(
+            // horizontal scrolling
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.purple,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
