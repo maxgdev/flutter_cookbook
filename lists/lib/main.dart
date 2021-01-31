96c9ba75745cbbf942b6f7dda6dbe456f52f7df4
maxgdev <max@maxgdev.net>
2021-01-31 16:15:25 +0000

    Created 'create_grid_list.dart' to demo creating a list grid

---
+++ lists/lib/main.dart
@@ -0,0 +1,39 @@
+// Create a grid list
+// https://flutter.dev/docs/cookbook/lists/grid-lists
+// create_grid_list.dart
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Grid List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 3,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
