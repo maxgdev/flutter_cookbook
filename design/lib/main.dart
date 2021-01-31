c1d8db2e4c262e35fdd1fb0fa67ddfad3142f62c
maxgdev <max@maxgdev.net>
2021-01-31 12:01:05 +0000

    Added google font package from pub.dev, imported fonts & used in example

--- design/lib/main.dart
+++ design/lib/main.dart
@@ -1,47 +1,34 @@
-// Display a snackbar
-// https://flutter.dev/docs/cookbook/design/snackbars
-// display_snackbar.dart
+// Export fonts from a package
+// https://flutter.dev/docs/cookbook/design/package-fonts
+// export_fonts_from_package.dart
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(SnackBarDemo());
+void main() => runApp(MyApp());
 
-class SnackBarDemo extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SnackBar Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('SnackBar Demo'),
-        ),
-        body: SnackBarPage(),
-      ),
+      title: 'Package Fonts',
+      home: MyHomePage(),
     );
   }
 }
 
-class SnackBarPage extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          final snackBar = SnackBar(
-            content: Text('Yes! A SnackBar??'),
-            action: SnackBarAction(
-              label: 'Undo',
-              onPressed: () {
-                // Some code to undo the change.
-              },
-            ),
-          );
-
-          // Find the Scaffold in the widget tree and use
-          // it to show a SnackBar.
-          Scaffold.of(context).showSnackBar(snackBar);
-        },
-        child: Text('Show SnackBar'),
+    return Scaffold(
+      // The AppBar uses the app-default font.
+      appBar: AppBar(title: Text('Package Fonts')),
+      body: Center(
+        // This Text widget uses the Raleway font.
+        child: Text(
+          'Using the Raleway font from the awesome_package',
+          style: GoogleFonts.lato(),
+        ),
       ),
     );
   }
